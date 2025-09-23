a78c7ba614df88c892624b1d6028a310d81b94e9
Nika-Soltani-Tehrani <nsoltanitehraniw@gmail.com>
2025-09-23 22:47:44 +0200

    redesigh

---
+++ exercises/Week2/InterfaceConceptExercise/src/Person.java
@@ -0,0 +1,29 @@
+public class Person implements MyComparable<Person> {
+
+    protected String name;
+    protected String surname;
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void printPersonName() {
+        System.out.println("Person's name is: " + this.name + ", " + this.surname);
+    }
+
+    @Override
+    public int compareTo(Person other) {
+        int surnameCmp = surname.compareToIgnoreCase(other.surname);
+        if (surnameCmp != 0) return surnameCmp;
+        return name.compareToIgnoreCase(other.name);
+    }
+}
